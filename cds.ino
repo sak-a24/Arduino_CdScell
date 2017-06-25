2be86f957ea9a22bc7fca0d02f3bcc8fe896d556
sak-a24 <hundary.kettary@gmail.com>
2017-06-25 10:39:01 +0900

    Add files via upload

---
+++ cds.ino
@@ -0,0 +1,24 @@
+const int CDS_PORT = A0;  // CdSセンサー入力ポート
+const int LED_PORT = 9;   // LED出力ポート
+const boolean SERIAL_OUTPUT = true; // 輝度のシリアル出力
+const int LOOP_DELAY = 200; // ループ周期(ms)
+
+
+void setup() {
+  pinMode(LED_PORT, OUTPUT);  // LEDポートを出力ポートにセット
+}
+
+void loop() {
+  int brightness = analogRead(CDS_PORT);  // センサーからのアナログデータを取得
+  int luminance = map(brightness, 0, 1023, 255, 0);
+
+  analogWrite(LED_PORT, luminance);
+  
+  if(SERIAL_OUTPUT){
+    Serial.begin(9600);
+    Serial.print(luminance);
+    Serial.println("");
+  }
+  
+  delay(LOOP_DELAY);
+}
